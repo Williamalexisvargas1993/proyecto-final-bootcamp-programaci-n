f55c85a58c6b7d2310a95debb131f4edd7babae1
Williamalexisvargas1993 <w.vargas@udea.edu.co>
2025-06-04 16:46:54 -0500

    modificaciones del 04/06/2025

--- README.m
+++ README.m
@@ -1,1 +1,9 @@
-zzzz
+se hizo el formulario en buzon
+se hizo la hoja de carbon
+se hizo la hoja de eolica
+se hizo la hoja de gas
+se hizo la hoja de nosotros
+se hizo la hoja de petroleo
+se hizo la hoja de solar
+se hizo la modificacción del target
+queda pendiente la seccion del hidrogeno verde y del css de la encuesta
